f78fd2849de21b27aa5bc2867b8d3e0473a5561f
Mohsen Farahani <methdfarahani@gmail.com>
2021-12-02 16:13:19 +0330

    Setup MainActivity

--- app/src/main/java/com/example/githubgraphql/MainActivity.kt
+++ app/src/main/java/com/example/githubgraphql/MainActivity.kt
@@ -3,36 +3,56 @@ package com.example.githubgraphql
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.paging.ExperimentalPagingApi
 import com.example.githubgraphql.ui.theme.GithubGraphQLTheme
+import com.example.githubgraphql.ui.view.Screens
+import com.example.githubgraphql.ui.view.profile.ProfileScreen
+import com.example.githubgraphql.ui.view.profile.ProfileViewModel
+import com.google.accompanist.pager.ExperimentalPagerApi
+import dagger.hilt.android.AndroidEntryPoint
 
+@ExperimentalPagerApi
+@ExperimentalPagingApi
+@ExperimentalMaterialApi
+@ExperimentalComposeUiApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             GithubGraphQLTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Screens.ProfileScreen.route,
+                    builder = {
+                        addProfileScreen(navController = navController)
+                    }
+                )
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    GithubGraphQLTheme {
-        Greeting("Android")
+@ExperimentalPagerApi
+@ExperimentalPagingApi
+@ExperimentalComposeUiApi
+@ExperimentalMaterialApi
+fun NavGraphBuilder.addProfileScreen(
+    navController: NavHostController,
+) {
+    composable(
+        route = Screens.ProfileScreen.route,
+    ) {
+        val viewModel: ProfileViewModel = hiltViewModel()
+        ProfileScreen(viewModel = viewModel)
     }
 }
