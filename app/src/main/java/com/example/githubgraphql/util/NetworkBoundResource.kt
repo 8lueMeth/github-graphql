1bde045f9e4fb4939d469bd47165a549a703ffc4
Mohsen Farahani <methdfarahani@gmail.com>
2021-12-02 16:15:07 +0330

    Implement NetworkBoundResource Function

---
+++ app/src/main/java/com/example/githubgraphql/util/NetworkBoundResource.kt
@@ -0,0 +1,29 @@
+package com.example.githubgraphql.util
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.*
+
+inline fun <ResultType, RequestType> networkBoundResource(
+    crossinline query: () -> Flow<ResultType>,
+    crossinline fetch: suspend () -> RequestType,
+    crossinline saveFetchResult: suspend (RequestType) -> Unit,
+    crossinline shouldFetch: (ResultType) -> Boolean = { true },
+) = flow {
+    val data = query().first()
+
+    val flow = if (shouldFetch(data)) {
+        emit(Resource.Loading(data))
+
+        try {
+            delay(1000)
+            saveFetchResult(fetch())
+            query().map { Resource.Success(it) }
+        } catch (throwable: Throwable) {
+            query().map { Resource.Error(throwable, it) }
+        }
+    } else {
+        query().map { Resource.Success(it) }
+    }
+
+    emitAll(flow)
+}
